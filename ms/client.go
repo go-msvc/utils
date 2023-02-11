14c7833741c78a01e70bcaf5d10f5ab8c7539e9f
Jan Semmelink <admin@adaptit.com>
2023-02-11 15:53:51 +0200

    Update

--- ms/client.go
+++ ms/client.go
@@ -1,12 +1,40 @@
 package ms
 
+import (
+	"context"
+	"time"
+
+	"github.com/go-msvc/errors"
+)
+
+type CtxID struct{}
+
 type Address struct {
 	Domain    string `json:"domain,omitempty" doc:"Domain name e.g. api.mystuff.co.za"`
 	Operation string `json:"operation,omitempty" doc:"Operation name within the domain"`
 }
 
+func (a Address) Validate() error {
+	if a.Domain == "" {
+		return errors.Errorf("missing domain")
+	}
+	if a.Operation == "" {
+		return errors.Errorf("missing operation")
+	}
+	return nil
+} //Address.Validate()
+
 type Client interface {
-	Sync(addr Address, req interface{}) (res interface{}, err error)
+	Sync(
+		ctx context.Context,
+		serviceAddress Address,
+		ttl time.Duration,
+		req interface{},
+	) (
+		res interface{},
+		err error,
+	)
+
 	ASync(addr Address, req interface{}) (err error)
-	Send(addr Address, req interface{}) (err error)
+	Send(addr Address, ttl time.Duration, req interface{}) (err error)
 }
